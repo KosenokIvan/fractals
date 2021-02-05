c45533cbeacf6948ec70ae419504aeec84045a82
Ivan <kosenoki213@gmail.com>
2021-02-05 18:17:58 +0300

    Добавлена возможность смены аксиомы и теорем

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import sys
 from math import sin, cos, radians
 from typing import Dict
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QVBoxLayout, QWidget
 from PyQt5.QtGui import QPainter, QPixmap, QImage, QPen, QColor
 from PyQt5 import uic
 from l_system import LSystem
+import constants as cst
 
 
-class FractalGenerator(QMainWindow):
+class FractalGeneratorWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.l_system_config_manager = LSystemConfigManager(self, "X",
+                                                            {"X": "-YF+XFX+FY-", "Y": "+XF-YFY-FX+"})
         self.parser = LStringParser("X", {"X": "-YF+XFX+FY-", "Y": "+XF-YFY-FX+"}, 16)
         self.image = QImage(512, 512, QImage.Format_ARGB32_Premultiplied)
         self.iteration = 0
         self.initUI()
 
     def initUI(self):
-        uic.loadUi("design.ui", self)
+        uic.loadUi("design/image_window.ui", self)
         self.next_iteration_btn.clicked.connect(self.next_iteration)
         self.previous_iteration_btn.clicked.connect(self.previous_iteration)
         self.save_image_action.triggered.connect(self.save_in_file)
+        self.configuration_action.triggered.connect(self.open_l_system_config_window)
         self.draw()
 
     def draw(self):
@@ -40,7 +44,125 @@ class FractalGenerator(QMainWindow):
 
     def save_in_file(self):
         filename = QFileDialog.getSaveFileName(self, "Выберите файл", "", "Изображение(*.png)")[0]
+        if not filename:
+            return
         self.image.save(filename)
+
+    def open_l_system_config_window(self):
+        window = LSystemConfigWindow(self, self.l_system_config_manager)
+        window.show()
+
+    def update_l_system_configuration(self):
+        self.parser = LStringParser(self.l_system_config_manager.get_axiom(),
+                                    self.l_system_config_manager.get_theorems(), 16)
+        self.iteration = 0
+        self.draw()
+
+
+class LSystemConfigManager:
+    def __init__(self, fr_generator: FractalGeneratorWindow, axiom: str, theorems: Dict[str, str]):
+        self.fr_generator = fr_generator
+        self.axiom = axiom
+        self.theorems = theorems.copy()
+
+    def get_axiom(self):
+        return self.axiom
+
+    def set_axiom(self, value: str):
+        self.axiom = value
+
+    def get_theorems(self):
+        return self.theorems.copy()
+
+    def get_theorem(self, key: str):
+        return self.theorems.get(key, key)
+
+    def set_theorems(self, theorems: Dict[str, str]):
+        self.theorems = theorems.copy()
+
+    def set_theorem(self, key: str, value: str):
+        self.theorems[key] = value
+
+    def update_generator_configuration(self):
+        self.fr_generator.update_l_system_configuration()
+
+
+class LSystemConfigWindow(QMainWindow):
+    def __init__(self, parent, manager: LSystemConfigManager):
+        super().__init__(parent)
+        self.manager = manager
+        self.theorems_list = TheoremsListWidget(self)
+        self.initUI()
+        self.theorems_sa.setWidget(self.theorems_list)
+
+    def initUI(self):
+        uic.loadUi("design/l_system_config_window.ui", self)
+        self.add_theorem_btn.clicked.connect(lambda: self.add_theorem())
+        self.confirm_btn.clicked.connect(self.update_manager)
+        self.axiom_input.setText(self.manager.get_axiom())
+        for th_input, th_output in self.manager.get_theorems().items():
+            self.add_theorem(th_input, th_output)
+
+    def add_theorem(self, th_input="", th_output=""):
+        self.theorems_list.add_theorem(th_input, th_output)
+
+    def update_manager(self):
+        axiom = self.axiom_input.text().strip()
+        if not axiom:
+            self.statusbar.showMessage("Не указана аксиома", cst.STATUS_BAR_TIMEOUT)
+            return
+        theorems = {}
+        for theorem_item in self.theorems_list.get_theorems():
+            theorem = theorem_item.widget()
+            th_input, th_output = map(str.strip, theorem.get_theorem())
+            if not th_input:
+                self.statusbar.showMessage("Не указаны входные данные теоремы!",
+                                           cst.STATUS_BAR_TIMEOUT)
+                return
+            if th_input in theorems:
+                self.statusbar.showMessage("Теоремы с одинаковыми входными данными",
+                                           cst.STATUS_BAR_TIMEOUT)
+                return
+            theorems[th_input] = th_output
+        self.manager.set_axiom(axiom)
+        self.manager.set_theorems(theorems)
+        self.manager.update_generator_configuration()
+        self.close()
+
+
+class TheoremsListWidget(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.theorems_layout = QVBoxLayout()
+        self.setLayout(self.theorems_layout)
+
+    def add_theorem(self, th_input="", th_output=""):
+        theorem_widget = TheoremWidget(self, th_input, th_output)
+        self.theorems_layout.addWidget(theorem_widget)
+
+    def get_theorems(self):
+        result = []
+        for i in range(len(self.theorems_layout)):
+            result.append(self.theorems_layout.itemAt(i))
+        return result
+
+
+class TheoremWidget(QWidget):
+    def __init__(self, parent, th_input="", th_output=""):
+        super().__init__(parent)
+        self.initUI()
+        self.theorem_input.setText(th_input)
+        self.theorem_output.setText(th_output)
+
+    def initUI(self):
+        uic.loadUi("design/theorem_widget.ui", self)
+        self.delete_theorem_btn.clicked.connect(self.delete)
+
+    def get_theorem(self):
+        return self.theorem_input.text(), self.theorem_output.text()
+
+    def delete(self):
+        self.setParent(None)
 
 
 class LStringParser(LSystem):
@@ -85,6 +207,6 @@ def excepthook(cls, value, traceback):
 if __name__ == '__main__':
     sys.excepthook = excepthook
     app = QApplication(sys.argv)
-    ex = FractalGenerator()
+    ex = FractalGeneratorWindow()
     ex.show()
     sys.exit(app.exec())
